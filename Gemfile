3a94a05521c90ca99fbe4d2011ff7ec17201a758
authentic <galaxy73rambler@yahoo.com>
2012-05-15 15:52:28 -0400

    static_pages controller

--- Gemfile
+++ Gemfile
@@ -10,9 +10,15 @@ gem 'rails', '3.2.3'
 group :development do
   gem 'mysql2'
   gem 'rspec-rails', '2.9.0'
+  gem 'guard-rspec', '0.5.5'
 end
 group :test do
+  gem 'rspec-rails', '2.9.0'
   gem 'capybara', '1.1.2'
+  gem 'rb-inotify', '0.8.8'
+  gem 'libnotify', '0.5.9'
+  gem 'guard-spork', '0.3.2'
+  gem 'spork', '0.9.0'
 end
 # Gems used only for assets and not required
 # in production environments by default.
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 MPOAWebSite::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,11 +50,11 @@ MPOAWebSite::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+   match ':controller(/:action(/:id))(.:format)'
 end
