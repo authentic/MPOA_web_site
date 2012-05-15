97f97a10b9021a77c185a9d4b7d4f92d18086435
authentic <galaxy73rambler@yahoo.com>
2012-05-15 11:35:53 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MPOAWebSite::Application.config.secret_token = 'b255e01bbff7c5ff01bf98369a6499635cd421b08632a5c8a447f09c66270230d954517ded9bf542d29ed54dac69b7d1e6ab2006d433a67c3531c3ba01df5500'
